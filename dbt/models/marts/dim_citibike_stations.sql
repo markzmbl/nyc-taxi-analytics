c44a920e17beeeb9e1ca4ba8439add101f7048ef
Markus Zimmball <markus@zimmball.de>
2026-06-23 09:31:40 +0200

    chore: remove legacy NYC Postgres dbt project (superseded by transform/)
    The pluggable blueprint uses transform/ (multi-engine). The old Postgres-targeted dbt project is dead; the slim NYC demo lives in transform/models/demo/.

--- dbt/models/marts/dim_citibike_stations.sql
+++
@@ -1,9 +0,0 @@
-select
-    station_id,
-    station_name,
-    latitude,
-    longitude,
-    capacity,
-    borough,
-    {{ map_to_taxi_zone('latitude', 'longitude') }} as nearest_taxi_zone_id
-from {{ ref('citibike_station_lookup') }}
--- dbt/models/marts/dim_taxi_zones.sql
+++
@@ -1,8 +0,0 @@
-select
-    location_id,
-    borough,
-    zone,
-    service_zone,
-    centroid_lat,
-    centroid_lon
-from {{ ref('taxi_zone_lookup') }}
